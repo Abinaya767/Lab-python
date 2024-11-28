2000053c44a8b4277fe87d98987ffd10d66d9d5f
Abinaya767 <abinayasaravanan2410@gmail.com>
2024-11-28 15:02:42 +0530

    Add files via upload

---
+++ lab--3.py
@@ -0,0 +1,15 @@
+#3 python lab 3
+leng=int(input("enter length="))
+bread=int(input("enter breadth="))
+area_rec=leng*bread
+print("area of rectangle= ",area_rec)
+base=int(input("enter the base of the triangle="))
+hei=int(input("enter the height of the triangle="))
+area_tri=0.5*base*hei
+print("area of triangle=",area_tri)
+side=int(input("enter the side of the square="))
+area_sq=side*side
+print("area of square=",area_sq)
+rad=int(input("enter the radius of the circle="))
+area_cir=3.14*rad*rad
+print("area of circle=",area_cir)
