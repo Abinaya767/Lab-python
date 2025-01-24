2dc9083321ca642033ff3434337ea338003e7b09
Abinaya767 <abinayasaravanan2410@gmail.com>
2025-01-24 06:26:00 +0530

    Update Lab--9.py

--- Lab--9.py
+++ Lab--9.py
@@ -3,10 +3,5 @@ dictionary={'1':10,'2':20,'3':50}
 total=sum(dictionary.values())
 print(total)
 
-#2
-inp=int(input())
-for i in range(1,inp+1):
-    for j in range(i):
-        print(i,end="")
-    print()
-     
+
+
